077ab214ca6897d459ded87db8d8ea8a59b6ad97
Cy-r0 <cirocursio@live.it>
2019-10-01 00:27:33 +0100

    added params for input and output dimensions

--- models/models.py
+++ models/models.py
@@ -5,11 +5,14 @@ import torch.nn as nn
 class SimpleCNN(nn.Module):
     """
     Simple 1D convolutional network.
+    Args:
+        input_features (int): number of input features.
+        output_classes (int): number of output classes.
     """
 
-    def __init__(self, n_features):
+    def __init__(self, input_features, output_classes):
         super().__init__()
-        self.conv1 = nn.Conv1d(n_features, 1, 3)
+        self.conv1 = nn.Conv1d(input_features, output_classes, 1)
 
     def forward(self, x):
         x = self.conv1(x)
