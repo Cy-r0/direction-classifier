87f4035cee57e5a1a315a820160122c420cac1f9
Cy-r0 <cirocursio@live.it>
2019-10-02 21:42:27 +0100

    added normalisation transform

--- transforms/transforms.py
+++ transforms/transforms.py
@@ -3,10 +3,49 @@ import torch
 
 class Normalise(object):
     """
-    Normalise features in sample between 0 and 1.
+    Normalise features in sample according to given mean and std.
+    Args:
+        mean (pandas DataFrame).
+        std (pandas DataFrame).
     """
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
     def __call__(self, datapoint):
-        #TODO: implement
+        sample = datapoint["sample"]
+        target = datapoint["target"]
+
+        # Reorder columns in stats to match order of sample
+        # Will throw an error if stats dont contain all the headers in sample
+        self.mean = self.mean[sample.columns]
+        self.std = self.std[sample.columns]
+
+        print(sample)
+
+        for i in range(len(mean.columns)):
+
+            # Calculate mean on the fly
+            if mean.iloc[:, i] == "auto":
+                automean = sample.iloc[:, i].mean()
+                sample.iloc[:, i] -= automean
+            # Use given mean
+            else:
+                sample.iloc[:, i] -= self.mean.at[0, i]
+
+            # Calculate std on the fly
+            if std.iloc[:, i] == "auto":
+                autostd = sample.iloc[:, i].std()
+                sample.iloc[:, i] /= autostd
+            # Use given std
+            else:
+                sample.iloc[:, i] /= self.std.at[0, i]
+        
+        print(sample)
+                
+
+
+        datapoint = {"sample": sample, "target": target}
         return datapoint
         
 
